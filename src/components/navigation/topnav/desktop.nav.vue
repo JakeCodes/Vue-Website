dd3ab5885778895956fde11ea0a6dab730454d15
EpixCodes <jakethesnake20006@gmail.com>
2020-09-26 13:35:54 -0500

    actually fixed navbar links

--- src/components/navigation/topnav/desktop.nav.vue
+++ src/components/navigation/topnav/desktop.nav.vue
@@ -1,26 +1,18 @@
 <template>
-  <nav id="nav" class="center">
-    <div id="nav-title">
-      <v-btn text class="nav-btn-title">
-        <a href="#about" v-smooth-scroll style="color: white">JakeCodes</a>
-      </v-btn>
-    </div>
-    <div class="nav-item">
-      <v-btn text class="nav-btn-item">
-        <a href="#portfolio" v-smooth-scroll style="color: white">Portfolio</a>
-      </v-btn>
-    </div>
-    <div class="nav-item">
-      <v-btn text class="nav-btn-item">
-        <a href="#skills" v-smooth-scroll style="color: white">Skills</a>
-      </v-btn>
-    </div>
-    <div class="nav-item">
-      <v-btn text class="nav-btn-item">
-        <a href="#contact" v-smooth-scroll style="color: white">Contact Me</a>
-      </v-btn>
-    </div>
-  </nav>
+  <div id="nav" class="center">
+    <a href="#about" v-smooth-scroll class="nav-link">
+      <v-btn text id="nav-title" class="nav-item">JakeCodes</v-btn>
+    </a>
+    <a href="#portfolio" v-smooth-scroll class="nav-link">
+      <v-btn text class="nav-item">Portfolio</v-btn>
+    </a>
+    <a href="#skills" v-smooth-scroll class="nav-link">
+      <v-btn text class="nav-item">Skills</v-btn>
+    </a>
+    <a href="#contact" v-smooth-scroll class="nav-link">
+      <v-btn text class="nav-item">Contact Me</v-btn>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -37,15 +29,15 @@ export default {
   height: 70px;
   z-index: 1000;
 }
-#nav-title,
-.nav-btn-title {
-  padding-right: 50px;
+#nav-title {
   font-size: 25px;
+}
+.nav-link {
+  margin-right: 30px;
 }
 .nav-item {
   color: white !important;
   text-decoration: none;
-  padding-right: 30px;
 }
 .nav-item:hover {
   color: #41474d !important;
